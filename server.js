0b0f4db761ee4208d7a280b94a334fdaad46f1f4
amirreza-dev <amirreza.dev@protonmail.com>
2025-08-04 14:10:05 +0330

    Create a middleware for getting ejs to work

--- server.js
+++ server.js
@@ -14,4 +14,8 @@ app.set('view engine', 'ejs');
 //?Static files
 setStatics(app);
 
+app.get('/', (req, res) => {
+  res.render('index', { pageTitle: 'اپلیکیشن کارهای روزمره' });
+});
+
 app.listen(3000, () => console.log(`Server is running`));
