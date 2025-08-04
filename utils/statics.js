3e42217645808f80ef3e550be7550854eff4d7a5
amirreza-dev <amirreza.dev@protonmail.com>
2025-08-04 14:18:59 +0330

    Public direction is fixed

--- utils/statics.js
+++ utils/statics.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 exports.setStatics = (app) => {
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '..', 'public')));
   app.use(
     express.static(
       path.join(__dirname, '..', 'node_modules', 'bootstrap-v4-rtl', 'dist')
