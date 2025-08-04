bf8effbdbe08d244fffcc3d92536143ba4ab67af
amirreza-dev <amirreza.dev@protonmail.com>
2025-08-04 13:18:19 +0330

    Project is created

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {
+  console.log('Server is Runing.');
+});
