d13065a4f67ac15812291720775e5d05611f5148
amirreza-dev <amirreza.dev@protonmail.com>
2025-08-05 19:59:49 +0330

    I've added my name my website address and a signature and also changed the page title

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const Todo = require('../model/todo');
 exports.getIndex = (req, res) => {
   Todo.fetchAll((todos) => {
     res.render('index', {
-      pageTitle: 'اپلیکیشن کارهای روزمره',
+      pageTitle: 'اولین نمونه کار بک اند - اپ کارهای روزمره',
       todos,
     });
   });
