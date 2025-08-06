917e12126a5da3a26caa3b7ec9930f434a4c6d32
amirreza-dev <amirreza.dev@protonmail.com>
2025-08-06 14:36:19 +0330

    Just created a 404 not found page and added to the project

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { setStatics } = require('./utils/statics');
 const adminRoutes = require('./routes/admin');
 const indexRouter = require('./routes/index');
+const notFoundController = require('./controllers/404');
 
 const app = express();
 
@@ -19,5 +20,8 @@ setStatics(app);
 //?Routes
 app.use(indexRouter);
 app.use('/admin', adminRoutes);
+
+//!404 Not Found
+app.use(notFoundController.get404);
 
 app.listen(3000, () => console.log(`Server is running on port 3000`));
